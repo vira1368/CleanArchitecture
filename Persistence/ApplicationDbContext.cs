e913d37a7e500a85832fc8acfe43642ba8db4694
Amin Saffarnejad <vira1368@gmail.com>
2021-12-21 13:08:18 +0330

    Added application database.

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+    }
+}
