2bd97c7c1b957715b5233d7c1567cbb1f2c5a535
Amin Saffarnejad <vira1368@gmail.com>
2021-12-21 14:27:47 +0330

    Added connection string to appsettings and use sql server service.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
